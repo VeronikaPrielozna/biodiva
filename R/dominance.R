e8fadf9c13a5e10caf5b48453482899cc355bf2b
VeronikaPrielozna <verca.prielozna@gmail.com>
2023-04-04 15:34:12 +0200

    Code changes

--- R/dominance.R
+++ R/dominance.R
@@ -1,31 +1,13 @@
-Dominance<-function(x, first.col = 2, table = 1, graph = 1, legl = 11, col = col, bty = "n", ...){
+Dominance<-function(x, first.col = 2, table = 1, graph = 1, legl = 12,
+                    textA = "Absolute proportion of species richness for classes of dominance.",
+                    textB = "Relative proportion of species richness for classes of dominance.",
+                    textA1 = "Total number of species",
+                    textB1 = "Relative frequency",
+                    col = c("gray0","gray48","gray78", "gray84", "gray90"), bty = "n", ...){
   calDom<-function(x) table(cut(x,breaks=c(0,2,5,12,40,100), labels = c("Sr", "R", "Sd.", "D", "E")))
   tableA<-apply(x[-1],2,calDom)
   tableR<-round(apply(tableA,2,function(tableA) tableA/sum(tableA)), digits = 2)
   maxA<-max(apply(tableA, 2, function(x) sum(x))) # max in tableA
-
-  textA<-"Absolute proportion of species richness for classes of dominance: "
-  textB<-"Relative proportion of species richness for classes of dominance: "
-  textA1<-"Total number of species"
-  textB1<-"Relative frequency"
-
-  if (table == 1){
-    cat(paste(textA, "\n\n"))
-    print(tableA)
-    cat("\n")
-    cat(paste(textB, "\n\n"))
-    print(tableR)
-  }
-
-  if (table == 2){
-    cat(paste(textA, "\n\n"))
-    print(tableA)
-  }
-
-  if (table == 3){
-    cat(paste(textB, "\n\n"))
-    print(tableR)
-  }
 
   if (graph == 1){
     par(mfrow=c(2,1), mar=c(3.8,4,2,1), las = 2)
@@ -50,4 +32,23 @@ Dominance<-function(x, first.col = 2, table = 1, graph = 1, legl = 11, col = col
             args.legend = list(x = legl, y = 1.2, bty = bty, ncol = 1, ncol = 6),
             col = col)
   }
+
+  if (table == 1){
+    table1 <-list( "Absolute proportion" = tableA, "Relative proportion" = tableR)
+    return(table1)
+
+  }
+
+  if (table == 2){
+    cat(paste(textA, "\n\n"))
+    return(tableA)
+  }
+
+  if (table == 3){
+    cat(paste(textB, "\n\n"))
+    return(tableR)
+  }
+
+
+
 }
