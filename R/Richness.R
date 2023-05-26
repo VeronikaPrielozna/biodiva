8758bcde76fc27c592926fad847704924d78f9fb
VeronikaPrielozna <verca.prielozna@gmail.com>
2023-05-26 13:35:47 +0200

    bug fix

--- R/Richness.R
+++ R/Richness.R
@@ -1,6 +1,8 @@
-richness<-function(x, first.col = 2, graph = T, legh = 10, legl = 10, col = c("gray0", "gray48", "gray84"), ylab = "Number of species", ncol = 3, ...){
-  x<-x[,first.col:ncol(x)]
+richness<-function(x, first.col = 2, graph = T, legh = 10, legl = 10,
+                   col = c("gray0", "gray48", "gray84"),
+                   ylab = "Number of species", lncol = 3, ...){
 
+  x<-x[,first.col:ncol(x)]
   F1<-apply(x, 2, function(f1) sum(f1==1))
   F2<-apply(x, 2, function(f2) sum(f2==2))
   Other<-apply(x, 2, function(f4) sum(f4>2))
@@ -10,15 +12,15 @@ richness<-function(x, first.col = 2, graph = T, legh = 10, legl = 10, col = c("g
   maxS<-max(SRtab[1,] + SRtab[2,])
   maxS2<-max(SRtab[4,])
 
-
   if (graph == T){
     par(mfrow=c(1,1), mar=c(4,4,2,0.5), las = 2)
     posgr = barplot(SRtab, plot = F)
-    barplot(SRtab[-4,], ylim = c(0,maxS2 + 5), ylab = ylab,
-            legend.text = c(as.expression(bquote('F'['1'])), as.expression(bquote('F'['2'])), "Other"), col=col,
-            args.legend = list(bty = "n", x = legl, y = maxS2 + legh, ncol = ncol))
+    barplot(SRtab[-4,], ylim = c(0,maxS2 * 1.2), xaxs="r",
+            legend.text = c(as.expression(bquote('F'['1'])), as.expression(bquote('F'['2'])),"Other"), col=col,
+            args.legend = list(bty = "n", x = legl, y = maxS2 * 1.2 + legh, ncol = lncol))
+    title(ylab=ylab, line=2.5)
     text(posgr, Total, lab=Total, cex=0.9, pos=3)
   }
-
   return(SRtab)
 }
+
